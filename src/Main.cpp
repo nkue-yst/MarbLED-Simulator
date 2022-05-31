74c037a42d007fb675f783ffec069bdced0e6fed
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-05-31 16:14:25 +0900

    First commit

---
+++ src/Main.cpp
@@ -0,0 +1,9 @@
+#include "Simulator.hpp"
+
+int main()
+{
+    int test;
+    std::cin >> test;
+    std::cout << test;
+}
+
---
+++ src/Simulator.hpp
@@ -0,0 +1,8 @@
+#include <opencv2/opencv.hpp>
+
+class Simulator
+{
+public:
+    Simulator();
+    ~Simulator();
+};
