21e2708fe93ac3f958bd9a584e8a53df7147d2c7
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-06-09 22:44:43 +0900

    Add receiving color data

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include "Socket.hpp"
 #include "Common.hpp"
 #include "Simulator.hpp"
@@ -5,11 +7,7 @@
 Socket::Socket(Simulator* simulator)
     :SimComponentBase(simulator)
 {
-    zmq::context_t ctx;
-    zmq::socket_t sock(ctx, zmq::socket_type::pull);
-    sock.bind("tcp://127.0.0.1:44100");
-
-    printLog("Init Socket", true);
+    printLog("Create socket component", true);
 }
 
 Socket::~Socket()
@@ -19,9 +17,23 @@ Socket::~Socket()
 
 void Socket::run()
 {
+    zmq::context_t ctx;
+    zmq::socket_t sock(ctx, zmq::socket_type::pull);
+    sock.bind("tcp://127.0.0.1:44100");
+
+    printLog("Init Socket", true);
+
+    zmq::message_t buf;
+
     while (!this->getParent()->getQuitFlag())
     {
-        std::cout << "--- Socket is running ---" << std::endl;
-        delay(50);
+        auto res = sock.recv(buf, zmq::recv_flags::none);
+        std::vector rgb_value(buf.data<uint8_t>(), buf.data<uint8_t>() + buf.size() / sizeof(uint8_t));  // ピクセル毎のRGB値を動的配列に格納する
+
+        // 受信したRGB値をそれぞれ出力する
+        for (auto color : rgb_value)
+            std::cout << color << std::endl;
+
+        std::cout << std::endl;
     }
 }
