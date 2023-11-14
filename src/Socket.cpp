0684e874faf76e1c931ad03cf6b6ed550b3adbb9
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2023-11-14 21:05:23 +0900

    [wip] RGBデータ受信処理の実装

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -8,6 +8,7 @@
 #include "Simulator.hpp"
 
 #include <zmq.hpp>
+#include <zmq_addon.hpp>
 
 Socket::Socket(Simulator* simulator)
     :SimComponentBase(simulator)
@@ -30,32 +31,34 @@ void Socket::run(std::string dest_ip)
 
     sub.connect(dest);
 
-    zmq::message_t buf;
+    // パネルの縦横の枚数を取得
+    int32_t w = this->getParent()->getLedWidth() / PANEL_WIDTH;
+    int32_t h = this->getParent()->getLedHeight() / PANEL_HEIGHT;
+
+    std::vector<zmq::message_t> msgs;
     while (!this->getParent()->getQuitFlag())
     {
-        auto res = sub.recv(buf, zmq::recv_flags::none);
-        res = sub.recv(buf, zmq::recv_flags::none);
 
-        std::vector color_vec(buf.data<uint8_t>(), buf.data<uint8_t>() + buf.size() / sizeof(uint8_t));
-
+        // パネルの縦横の枚数分のデータを受信する
         uint32_t i = 0;
-        for (uint32_t y = 0; y < this->getParent()->getLedHeight(); y++)
+        for (int y = 0; y < h; ++y)
         {
-            for (uint32_t x = 0; x < this->getParent()->getLedWidth(); x++)
+            for (int x = 0; x < w; ++x)
             {
-                try
-                {
-                    this->getParent()->color_mat_[y * this->getParent()->getLedWidth() + x]
-                        = Color(
-                            color_vec[i++],
-                            color_vec[i++],
-                            color_vec[i++]
-                        );
-                }
-                catch(const std::exception& e)
-                {
-                    std::cerr << e.what() << '\n';
-                }
+                char filter[256] = {};
+                std::sprintf(filter, "BRD_COLOR %d", i++);
+                sub.setsockopt(ZMQ_SUBSCRIBE, filter, std::strlen(filter));
+
+                // Debug print for filter
+                // std::cout << filter << std::endl;
+
+                zmq::recv_multipart(sub, std::back_inserter(msgs), zmq::recv_flags::none);
+
+                // Debug print for received data
+                // std::cout << msgs[0].to_string() << std::endl;
+                // std::cout << msgs[1].to_string() << std::endl;
+                // std::cout << msgs[2].to_string() << std::endl;
+                // std::cout << msgs[3].to_string() << std::endl;
             }
         }
     }
